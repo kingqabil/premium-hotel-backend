3362dbc08673e52aa02cf49e622ba0ceb56596c4
SadiQ HabiL <kingqabil@live.com>
2022-05-25 21:40:44 +0300

    Added Rooms Model

---
+++ app/models/room.rb
@@ -0,0 +1,7 @@
+class Room < ApplicationRecord
+  validates :name, :city, length: { maximum: 50 }
+  validates :name, :city, :rate, :type, :amenities, :picture, presence: true
+  validates :rate, numericality: { greater_than_or_equal_to: 0 }
+
+  belongs_to :user
+end
