60644ae1f8720bfba60124ce56999edde7b1f06c
SadiQ HabiL <kingqabil@live.com>
2022-05-25 22:19:14 +0300

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, defaults: { format: :json }, path: '',
+                     path_names: { sign_in: 'login', sign_out: 'logout', registration: 'signup' }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  defaults format :json do
+    resources :rooms, only: %i[create index show update destroy]
 end
