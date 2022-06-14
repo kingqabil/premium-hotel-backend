71cd1135d592dbc37a09b70448fc4379fe9a9f4c
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-06-14 09:16:44 +0200

    correct linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,9 @@ class ApplicationController < ActionController::API
   end
 
   private
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[name email password password_confirmation])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,4 @@ class User < ApplicationRecord
 
   has_many :reservations, dependent: :destroy
   has_many :rooms, dependent: :destroy
-
 end
