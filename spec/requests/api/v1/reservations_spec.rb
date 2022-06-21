71a33c3da4209522b8c4ae6c54c993aacd209a56
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-06-21 14:17:55 +0200

    fix reservations specs

--- spec/requests/api/v1/reservations_spec.rb
+++ spec/requests/api/v1/reservations_spec.rb
@@ -2,24 +2,46 @@ require 'swagger_helper'
 
 RSpec.describe 'api/v1/reservations', type: :request do
   # rubocop: disable Metrics
+  before :all do
+    @user = create(:user)
+    post '/api/v1/users/signup', params: {
+      user: {
+        name: @user.name,
+        email: @user.email,
+        password: @user.password,
+        password_confirmation: @user.password
+      }
+    }, as: :json
+
+    post '/api/v1/users/login', params: {
+      user: {
+        email: @user.email,
+        password: @user.password
+      }
+    }, as: :json
+
+    @token = json['token']
+  end
+  before :each do
+    @room = create(:room, user: @user)
+  end
+
   path '/api/v1/reservations' do
     get('list reservations') do
       tags 'Reservations'
       security [bearer_auth: []]
 
       response(200, 'successful') do
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        let(:Authorization) { @token }
+        it 'Return  response' do
+          body = JSON(response.body)
+          expect(body.count).to eq(0)
         end
         run_test!
       end
 
-      response '201', 'Authorized' do
-        let(:Authorization) { "Bearer #{::Base64.strict_encode64('sadiq@gmail.com:111111')}" }
+      response '200', 'ok' do
+        let(:Authorization) { @token }
         run_test!
       end
 
@@ -44,33 +66,31 @@ RSpec.describe 'api/v1/reservations', type: :request do
       }
 
       response '201', 'reservation created' do
+        let(:Authorization) { @token }
         let(:reservation) do
-          { check_in: '2022-01-26', check_out: '2022-01-29', room_id: 1 }
+          { check_in: '2022-12-12', check_out: '2022-12-14', room_id: @room.id }
         end
         run_test!
       end
 
       response '422', 'invalid request' do
+        let(:Authorization) { @token }
         let(:reservation) { { check_in: '2022-01-26', check_out: '2022-01-29' } }
         run_test!
       end
 
       response '201', 'successfully authenticated' do
-        let(:Authorization) { "Bearer #{::Base64.strict_encode64('sadiq@gmail.com:111111')}" }
+        let(:Authorization) { @token }
+        let(:reservation) do
+          { check_in: '2022-12-12', check_out: '2022-12-14', room_id: @room.id }
+        end
         run_test!
       end
 
       response '401', 'authentication failed' do
         let(:Authorization) { "Bearer #{::Base64.strict_encode64('bogus:bogus')}" }
-        run_test!
-      end
-      response(200, 'successful') do
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
+        let(:reservation) do
+          { check_in: '2022-12-12', check_out: '2022-12-14', room_id: @room.id }
         end
         run_test!
       end
@@ -78,58 +98,30 @@ RSpec.describe 'api/v1/reservations', type: :request do
   end
 
   path '/api/v1/reservations/{id}' do
+    before :each do
+      @reservation = create(:reservation, room: @room, user: @user)
+    end
     # You'll want to customize the parameter types...
     parameter name: 'id', in: :path, type: :string, description: 'id'
-
-    get('show reservation') do
-      tags 'Reservations'
-      security [bearer_auth: []]
-      response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
-        run_test!
-      end
-      response '201', 'successfully authenticated' do
-        let(:Authorization) { "Bearer #{::Base64.strict_encode64('sadiq@gmail.com:111111')}" }
-        run_test!
-      end
-
-      response '401', 'authentication failed' do
-        let(:Authorization) { "Bearer #{::Base64.strict_encode64('bogus:bogus')}" }
-        run_test!
-      end
-    end
 
     delete('delete reservation') do
       tags 'Reservations'
       security [bearer_auth: []]
       response(200, 'successful') do
-        let(:id) { '123' }
-
-        after do |example|
-          example.metadata[:response][:content] = {
-            'application/json' => {
-              example: JSON.parse(response.body, symbolize_names: true)
-            }
-          }
-        end
+        let(:Authorization) { @token }
+        let(:id) { @reservation.id }
         run_test!
       end
 
-      response '201', 'delete reservation' do
-        let(:Authorization) { "Bearer #{::Base64.strict_encode64('sadiq@gmail.com:111111')}" }
+      response '200', 'delete reservation' do
+        let(:Authorization) { @token }
+        let(:id) { @reservation.id }
         run_test!
       end
 
       response '401', 'authentication failed' do
         let(:Authorization) { "Bearer #{::Base64.strict_encode64('bogus:bogus')}" }
+        let(:id) { @reservation.id }
         run_test!
       end
     end
